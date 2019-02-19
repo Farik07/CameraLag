5a991771fdb17a3117b4a04f8f1fd3e697999598
Farik07 <38301192+Farik07@users.noreply.github.com>
2019-02-19 11:46:41 -0600

    Ya pide los permisos correctamente, pero falta agregar un aviso diciendo que son necesarios para su correcto funcionamiento

--- app/src/main/java/com/nightmaredevs/cameralag/MainActivity.java
+++ app/src/main/java/com/nightmaredevs/cameralag/MainActivity.java
@@ -1,17 +1,32 @@
 package com.nightmaredevs.cameralag;
 
 
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        verifyPermission();
     }
+
+    private void verifyPermission(){
+
+        String [] permissions = {Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE};
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),permissions[0])!=PackageManager.PERMISSION_GRANTED
+                || ContextCompat.checkSelfPermission(this.getApplicationContext(),permissions[1])!=PackageManager.PERMISSION_GRANTED ){
+
+            ActivityCompat.requestPermissions(MainActivity.this, permissions,1);
+        }
+    }
+
 }
